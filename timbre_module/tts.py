29bc1a1ba16f23cd6aa7a0fa5007ed1746e116b1
thegksingh <gksingh1106@gmail.com>
2025-12-26 23:30:19 +0530

    wrote the tts class and streaming logic

--- timbre_module/tts.py
+++ timbre_module/tts.py
@@ -1,2 +1,34 @@
-import egde_tts
+import edge_tts
 import asyncio
+
+class Tts:
+    def __init__(self):
+        pass
+
+    async def generate_tts(
+        self,
+        tts_input_text,
+        voice,
+        rate = "+0%",
+        pitch = "+0Hz"
+    ):
+        #initializing egde_tts communnicate object with parameters(text,voice,rate,picth)
+        communicate = edge_tts.Communicate(
+            text = tts_input_text,
+            voice = voice,
+            rate = rate,
+            pitch = pitch
+        )
+            
+        #b"" is empty binary data that is store audio 
+        audio_data = b""
+
+        #recieving audio data in small chunks
+        async for chunk in communicate.stream():
+            if chunk["type"] == "audio":
+                #appending each chunk to audio_data
+                audio_data += chunk["data"]
+            
+        #reurning the complete audio_data after all chunks are recieved
+        return audio_data
+                    
