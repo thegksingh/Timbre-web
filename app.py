9514a132f360f7c43ecb9d83eeb56b268eff9a0d
thegksingh <gksingh1106@gmail.com>
2025-12-27 23:56:49 +0530

    added stt ui in app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import streamlit as st
 from timbre_module.tts import Tts
 import asyncio
-
+from timbre_module.stt import Stt
 
 @st.cache_resource
 def load_tts_service():
     return Tts()
 
+def load_stt_service(model="small"):
+    return Stt(model)
+    
+
 #initializing Tts class
-speech = load_tts_service() 
+tts_service = load_tts_service() 
+
+#initializing Stt class 
+stt_service = load_stt_service()
 
 def main():
     
@@ -26,6 +33,8 @@ def main():
    )
 
     #handling user choice
+
+    #Text-to-speech
     if users_input == "Text to Speech":
         st.markdown("<h3 style='color:#00E5FF;'>Text to speech service</h3>", unsafe_allow_html=True)
 
@@ -41,14 +50,6 @@ def main():
             "Enter voice:",
             value = "en-AU-NatashaNeural"
         )
-        
-        #error handling for empty text input and voice
-        if not tts_input_text.strip():
-            st.error("Please enter text to convert.")
-            st.stop()
-        if not voice.strip():
-            st.error("Please enter a voice.")
-            st.stop()
 
         #rate and pitch selection with sliders
         col1, col2 = st.columns(2)
@@ -86,12 +87,21 @@ def main():
             "Generate Speech",
             type = "primary",
             width = "stretch"
-        ):
-
+        ):  
+            #error handling for empty text input and voice
+            if not tts_input_text.strip():
+                st.error("Please enter text to convert.")
+                st.stop()
+            if not voice.strip():
+                st.error("Please enter a voice.")
+                st.stop()
+            
+            #error handling 
             try:
+                #showing spinner while generaitng speech
                 with st.spinner("Generating speech, please wait..."):
                     output_audio = asyncio.run(
-                        speech.generate_tts(
+                        tts_service.generate_tts(
                             text = tts_input_text,
                             voice = voice,
                             rate = rate,
@@ -104,8 +114,12 @@ def main():
                        output_audio,
                        format = "audio/wav"
                     )
-
+                    
+                    #success message
                     st.success("Speech generated successfully!")
+                    
+                    #celebrate TTS succss using ballons
+                    st.balloons()
 
                     #download button
                     st.download_button(
@@ -118,6 +132,51 @@ def main():
                     )
             except Exception as e:
                 st.error(f"An error occurred during speech generation: {e}")
+    
+   
+    #speech to text
+    if users_input == "Speech to Text":
+
+        st.markdown("<h3 style='color:#00E5FF;'>Speech to text service</h3>", unsafe_allow_html=True)
+        
+        #storing uploded file into varaible into input_audio
+        input_audio = st.file_uploader("Upload audio file:", type=["wav","mp3","m4a"])
+        
+        #generate button and handling generation
+        if st.button(
+                "Generate Text",
+                type = "primary",
+                width = "stretch"
+            ):
+
+            #error handling for empty file upload
+            if not input_audio:
+                st.error("Please upload an audio file.")
+                st.stop()
+ 
+            #error handling
+            try:
+                
+                #showing spinner while generating text
+                with st.spinner("Generating text, please wait..."):
+                    stt_output_text = stt_service.generate(input_audio)
+
+                    if stt_output_text:
+                        st.text_area(
+                            "Transcribed Text:",
+                            value = stt_output_text,
+                            height = 200
+                        )
+                    
+                        #success message
+                        st.success("Text generated successfully!")
+
+                        #celebrating STT success using snow
+                        st.snow()
+
+            except Exception as e:
+                st.error(f"An error occurred during text generation: {e}")
+
 
 if __name__ == "__main__":
     main()
