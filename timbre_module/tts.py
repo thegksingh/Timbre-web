d2cf84a9dfbe9724e69690f919fcbe038658a84d
thegksingh <gksingh1106@gmail.com>
2025-12-27 22:10:26 +0530

    added error handling and validation to tts logic

--- timbre_module/tts.py
+++ timbre_module/tts.py
@@ -1,3 +1,8 @@
+"""
+Module for Text-to-speech conversion using edge-tts
+"""
+
+
 import edge_tts
 import asyncio
 
@@ -7,28 +12,43 @@ class Tts:
 
     async def generate_tts(
         self,
-        tts_input_text,
+        text,
         voice,
         rate = "+0%",
         pitch = "+0Hz"
     ):
-        #initializing egde_tts communnicate object with parameters(text,voice,rate,picth)
-        communicate = edge_tts.Communicate(
-            text = tts_input_text,
-            voice = voice,
-            rate = rate,
-            pitch = pitch
-        )
+        
+        #basic validation
+        if not text.strip():
+            raise ValueError("Input text for TTS is empty.")
+        if not voice:
+            raise ValueError("Voice parameter for TTS is empty.")
+        
+        #error handling
+        try:
+            #initializing egde_tts communnicate object with parameters(text,voice,rate,picth)
+            communicate = edge_tts.Communicate(
+                text = text,
+                voice = voice,
+                rate = rate,
+                pitch = pitch
+            )
             
-        #b"" is empty binary data that is store audio 
-        audio_data = b""
+            #b"" is empty binary data that is store audio 
+            audio_data = b""
 
-        #recieving audio data in small chunks
-        async for chunk in communicate.stream():
-            if chunk["type"] == "audio":
-                #appending each chunk to audio_data
-                audio_data += chunk["data"]
+            #recieving audio data in small chunks
+            async for chunk in communicate.stream():
+                if chunk["type"] == "audio":
+                    #appending each chunk to audio_data
+                    audio_data += chunk["data"]
             
-        #reurning the complete audio_data after all chunks are recieved
-        return audio_data
-                    
+            #receiving the complete audio_data after all chunks are recieved
+            return audio_data
+        
+        #raise exception when request was succeeded but no audio was received
+        except edge_tts.exceptions.NoAudioReceived:
+            raise RuntimeError9Edge("TTS failed to generate audio.")
+
+        except Exception as e:
+            raise RuntimeError(f"TTS generation failed: {e}")           
