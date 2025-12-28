0b5554f64e639caff9610292be0b2e0e016e198b
thegksingh <gksingh1106@gmail.com>
2025-12-28 13:04:43 +0530

    finished the text polisher ui and integrated gemini

--- app.py
+++ app.py
@@ -2,14 +2,23 @@ import streamlit as st
 from timbre_module.tts import Tts
 import asyncio
 from timbre_module.stt import Stt
+from timbre_module.polisher import Polisher
+
 
 @st.cache_resource
 def load_tts_service():
     return Tts()
 
+@st.cache_resource
 def load_stt_service(model="small"):
     return Stt(model)
-    
+
+@st.cache_resource
+def load_polishser_service(model="gemini-2.5-flash"):
+    return Polisher(model)  
+
+#initializing Polisher class
+polisher_service = load_polisher_service()
 
 #initializing Tts class
 tts_service = load_tts_service() 
@@ -29,7 +38,8 @@ def main():
             "Speech to Speech",
             "Speech to Text",
             "Text Polisher"
-       ]
+       ],
+       index = None
    )
 
     #handling user choice
@@ -41,7 +51,7 @@ def main():
         #text to convert
         tts_input_text = st.text_area(
             "Enter text to convert to speech:",
-            max_chars = 5000,
+            max_chars = 20000,
             value = "Welcome to TIMBRE, your all-in-one AI-powered audio and text processing platform!"
         )
         #voice slection with default value 
@@ -177,6 +187,126 @@ def main():
             except Exception as e:
                 st.error(f"An error occurred during text generation: {e}")
 
+    
+    #text polisher
+    if users_input ==  "Text Polisher":
+        
+        user_choice = st.selectbox("Choose service:", options=["Translator","Enhancer"],index = None)
+        
+        #handling user_choice for translator
+        if user_choice == "Translator":
 
+            #getting user text
+            translation_input_text = st.text_area("Enter text for translation:")
+            
+            #getting target language
+            target_language = st.text_input("Enter target language:")
+
+            #generate button and handling translation
+            if st.button(
+                "Translate",
+                type = "primary",
+                width = "stretch"
+            ):
+                #error handling for empty text and language
+                if not translation_input_text.strip():
+                    st.error("Please enter text for translation")
+                    st.stop()
+                if not target_language.strip():
+                    st.error("Language can't be empty")
+                    st.stop()
+
+                #error handling   
+                try:
+
+                #translating text to target language and detecting valid language
+                    translator_result = polisher_service.translator(
+                        text = translation_input_text,
+                        language = target_language
+                    )
+                    
+                    #print result
+                    if translator_result:
+                        st.text_area(
+                            "Translation result",
+                            value = translator_result,
+                            height = 200
+                        )
+                        
+                        #success message
+                        st.success("Text translated.")
+                        
+                        #celebrating translation using snow
+                        st.snow()
+
+                except Exception as e:
+                    st.error(f"An error occured during translaton: {e}")
+            
+        #handling user_choicce fro enhancer
+        elif user_choice == "Enhancer":
+
+            #getting user text
+            enhancer_input_text = st.text_area("Enter text for enhacement:")
+
+            #getting target lstyle
+            target_style = st.selectbox(
+                "Select style:",
+                options = [
+                    "Professional",
+                    "Conversational",
+                    "Clear and Simple",
+                    "Concise",
+                    "Persuasive",
+                     "Storytelling",
+                    "Creative",
+                    "Empathetic",
+                    "Motivational",
+                    "Humorous",
+                    "Academic",
+                    "Technical",
+                ],
+                index = None
+            )
+           
+           #generate button and handling enhancer
+            if st.button(
+                "Enhance",
+                type = "primary",
+                width = "stretch"
+            ):
+                #ensure style is never None (selectbox returns None if not selected)
+                if target_style is None:
+                    target_style = ""
+
+                #error handling fro empty text and style
+                if not enhancer_input_text.strip():
+                    st.error("Please enter text for enhancement")
+                    st.stop() 
+                
+                #error handling
+                try:
+                    
+                    #enchancing text 
+                    enhancer_result = polisher_service.enhancer(
+                        text = enhancer_input_text,
+                        style = target_style
+                    )
+                    
+                    #print result
+                    if enhancer_result:
+                        st.text_area(
+                            "Enhanced text",
+                            value = enhancer_result,
+                            height = 200
+                        )
+                        
+                        #success message
+                        st.success("Text enhanced.")
+
+                        #celebrating enhancement using balloons
+                        st.balloons()
+
+                except Exception as e:
+                    st.error(f"An error occured during enhancement: {e}")
 if __name__ == "__main__":
     main()
